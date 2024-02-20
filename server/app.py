3e33045ce194b8db8cdd29d69be2c15ea1e2810a
Will Cooley <william.cooley@gmail.coom>
2024-02-20 12:26:04 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,38 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+
+        data = request.get_json()
+        record = Plant.query.filter(Plant.id == id).first()
+        for attr in data:
+            setattr(record, attr, data.get(attr))
+
+        db.session.add(record)
+        db.session.commit()
+
+        record_dict = record.to_dict()
+
+        response = make_response(
+            record_dict,
+            200
+        )
+
+        return response
+
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response = make_response(
+            '',
+            204
+        )
+
+        return response
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
